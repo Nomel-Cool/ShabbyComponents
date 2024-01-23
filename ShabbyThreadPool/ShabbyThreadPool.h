ae9e7baa800a66ae7da2d2e1c5f0ad7fc469c5aa
Nomel-Cool <619471786@qq.com>
2024-01-23 21:41:03 +0800

    first commit

---
+++ ShabbyThreadPool/ShabbyThreadPool.h
@@ -0,0 +1,68 @@
+#pragma once
+#ifndef SHABBY_THREAD_POOL_H
+#define SHABBY_THREAD_POOL_H
+
+#include <thread>
+#include <algorithm>
+#include <functional>
+#include <memory>
+
+#include "QuestType.h"
+
+class QuestNode
+{
+public:
+	QuestNode():isUsing(false),isTerminated(false)
+	{
+		quest_node = new QuestType();
+	}
+
+	QuestType* GetInstance()
+	{
+		if (quest_node != nullptr)
+			return quest_node;
+		return nullptr;
+	}
+	bool isUsing;
+	bool isTerminated;
+private:
+	QuestType* quest_node;
+
+};
+
+/// <summary>
+/// 一个简陋的线程池：
+/// 组成：
+/// 一个有限线程元容器
+/// 一个无限的任务队列
+/// 操作：
+/// 任务以队列方式与线程元关联后被消费
+/// 线程元被任务消费过程中需要加锁，同时任务完成前也需要加锁
+/// 
+/// </summary>
+class ShabbyThreadPool
+{
+public:
+	ShabbyThreadPool()
+	{
+		node = new QuestNode();
+	}
+	~ShabbyThreadPool()
+	{
+
+	}
+	QuestNode* GetInstance()
+	{
+		if (node != nullptr)
+			return node;
+		return nullptr;
+	}
+	bool AddQuestWithThread()
+	{
+
+	}
+private:
+	QuestNode *node;
+};
+
+#endif // !SHABBY_THREAD_POOL_H
---
+++ ShabbyThreadPool/test.cpp
@@ -0,0 +1,13 @@
+#include "ShabbyThreadPool.h"
+#include <string>
+
+int main()
+{
+    ShabbyThreadPool* node = new ShabbyThreadPool();
+    auto func_set = node->GetInstance()->GetInstance();
+
+    func_set->SetFunctor<void, int, int>("ADD", std::function<void(int,int)>([](int a, int b) { printf(std::to_string(a + b).c_str()); }));
+    func_set->GetFunctor<void, int, int>("ADD")(3, 4);
+
+    return 0;
+}
