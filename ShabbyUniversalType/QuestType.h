e7712e7c2345cdaf50a82d0bcad6f78a5a67d04c
Nomel-Cool <619471786@qq.com>
2024-01-26 11:39:41 +0800

    QuestType加入代理调用功能，用户需给出函数地址以及返回值

--- ShabbyUniversalType/QuestType.h
+++ ShabbyUniversalType/QuestType.h
@@ -41,12 +41,30 @@ public:
     }
 
     /// <summary>
-    /// 通过函子名称获取封装的函数表达式
+    /// 获取异步
+    /// </summary>
+    /// <param name="func_id">函子标签id</param>
+    /// <returns>可委托调用函数</returns>
+    auto GetAsyncFunctor(std::string func_id)
+    {
+        try
+        {
+            return quest_type[func_id];
+        }
+        catch (std::bad_any_cast& ex)
+        {
+            // 处理异常
+            throw ex;
+        }
+    }
+
+    /// <summary>
+    /// 通过函子特征获取封装的函数表达式
     /// (TODO)应当使用枚举体加映射获取函子ID，这样做的好处是可以哈希保证唯一性
     /// 这里的转换并不安全，前提是你必须清楚函子ID对应的函数结构，否则就会抛出异常
     /// </summary>
-    /// <typeparam name="RETURN_TYPE">模板参数: 返回值类型</typeparam>
-    /// <typeparam name="...ARGS_TYPE">模板参数: 形参类型</typeparam>
+    /// <typeparam name="RETURN_TYPE">返回值类型</typeparam>
+    /// <typeparam name="...ARGS_TYPE">形参类型</typeparam>
     /// <param name="func_id">函子在map中的key值</param>
     /// <returns>可调用函数</returns>
     template<typename RETURN_TYPE, typename... ARGS_TYPE>
@@ -55,6 +73,27 @@ public:
         try
         {
             return std::any_cast<std::function<RETURN_TYPE(ARGS_TYPE...)>>(quest_type[func_id]);
+        }
+        catch (std::bad_any_cast& ex)
+        {
+            // 处理异常
+            throw ex;
+        }
+    }
+
+    /// <summary>
+    /// 通过函子特征获取封装的函数表达式
+    /// </summary>
+    /// <typeparam name="RETURN_TYPE">返回值类型</typeparam>
+    /// <typeparam name="...ARGS_TYPE">形参类型</typeparam>
+    /// <param name="func_id">函子在map中的key值</param>
+    /// <returns>可委托调用函数</returns>
+    template<typename RETURN_TYPE, typename... ARGS_TYPE>
+    std::promise<RETURN_TYPE(ARGS_TYPE...)> GetAsyncFunctor(std::string func_id)
+    {
+        try
+        {
+            return std::any_cast<std::promise<RETURN_TYPE(ARGS_TYPE...)>>(quest_type[func_id]);
         }
         catch (std::bad_any_cast& ex)
         {
@@ -83,10 +122,10 @@ public:
     /// <typeparam name="...ARGS_TYPE">函子参数列表类型</typeparam>
     /// <param name="any_asyncfunc">函子在map中的key值</param>
     /// <returns></returns>
-    template<typename RETURN_TYPE, typename... ARGS_TYPE>
-    std::future<RETURN_TYPE(ARGS_TYPE...)> CastAnyToFuture(std::any any_asyncfunc)
+    template<typename RETURN_TYPE>
+    auto CastAnyToPromise(std::any any_asyncfunc)
     {
-        return std::any_cast<std::future<RETURN_TYPE(ARGS_TYPE...)>>(any_asyncfunc);
+        return std::any_cast<std::shared_ptr<std::packaged_task<RETURN_TYPE()>>>(any_asyncfunc);
     }
 
     /// <summary>
@@ -101,23 +140,28 @@ public:
     {
         quest_type[func_id] = func;
     }
-
+    
     /// <summary>
-    /// 鉴于函子的使用还有异步，因此使用std::promise负责签发函子的操作结果作为std::future
+    /// 用户将设置一个std::bind用于延迟计算，并及时签发一个future
     /// </summary>
     /// <typeparam name="RETURN_TYPE">函子返回值类型</typeparam>
     /// <typeparam name="...ARGS_TYPE">函子参数列表类型</typeparam>
     /// <param name="func_id">函子在map中的key值</param>
-    /// <param name="any_asyncfunc">函数本身，建议填写lambda</param>
+    /// <param name="func">函数本身，建议填写lambda</param>
+    /// <returns>封装好延迟计算的函子</returns>
     template<typename RETURN_TYPE, typename... ARGS_TYPE>
-    void SetAsyncFunctor(std::string func_id, std::promise<RETURN_TYPE(ARGS_TYPE...)> any_asyncfunc)
+    auto SetAsyncFunctor(std::string func_id, std::function<RETURN_TYPE(ARGS_TYPE...)> async_func, ARGS_TYPE... args)
     {
-        quest_type[func_id] = any_asyncfunc;
+        //由于是延迟计算，只告诉了委托方调用时使用的参数，然后在未来获取计算值即可
+        auto task = std::make_shared<std::packaged_task<RETURN_TYPE()>>(std::bind(async_func, args...));
+        auto future = task->get_future();
+        quest_type[func_id] = task;
+        return future;
     }
 
 
 private:
-    std::map<std::string, std::any> quest_type;
+    std::unordered_map<std::string, std::any> quest_type;
 };
 
 #endif // !QUEST_TYPE_H
--- ShabbyUniversalType/quest_type_test.cpp
+++ ShabbyUniversalType/quest_type_test.cpp
@@ -1,11 +1,29 @@
+#include <iostream>
+
 #include "QuestType.h"
+
+int add(int a, int b)
+{
+	return a + b;
+}
 
 int main()
 {
 	QuestType func_set;
-	func_set.SetFunctor<void, int, int>("ADD", std::function<void(int, int)>([=](int a, int b) { printf((std::to_string(a + b) + "\n").c_str()); }));
-	std::any any_add = func_set.GetFunctor("ADD");
-	auto f = func_set.CastAnyToFunction<void,int,int>(any_add);
-	f(1, 2);
+	//func_set.SetFunctor<void, int, int>("ADD", std::function<void(int, int)>([=](int a, int b) { printf((std::to_string(a + b) + "\n").c_str()); }));
+	//std::any any_add = func_set.GetFunctor("ADD");
+	//auto f = func_set.CastAnyToFunction<void,int,int>(any_add);
+	//f(1, 2);
+	
+	auto future = func_set.SetAsyncFunctor<int, int, int>("AsyncADD", std::function<int(int, int)>(add), 3, 4);
+
+	auto any_pm = func_set.GetAsyncFunctor("AsyncADD");
+
+	auto pm = func_set.CastAnyToPromise<int>(any_pm);
+
+	(*pm)();
+
+	std::cout << future.get() << std::endl;
+
 	return 0;
 }
