96388dfe5b06a4e72b94146a0ab57a9d5984ebcc
Nomel-Cool <619471786@qq.com>
2024-02-03 18:43:48 +0800

    添加了模板二叉树,后续应当使用智能指针替换所有动态指针完成内存管理

--- ShabbyDataStructure/BinaryTree.h
+++ ShabbyDataStructure/BinaryTree.h
@@ -16,91 +16,103 @@ namespace shabby
 	class TreeNode
 	{
 	public:
-		TreeNode(T data):Data(data) {}
+		TreeNode() {}
 		virtual ~TreeNode() {}
-		virtual TreeNode* GetLeftChild()
+		bool operator>(const TreeNode<T>& _other) const
+		{
+			return Data > _other.Data;
+		}
+		virtual TreeNode<T>* GetLeftChild() // Wtf，返回非模板的TreeNode也是能编译过去的？
 		{
 			return left_node;
-		}
-		virtual TreeNode* GetRightChild()
+		}  
+		virtual TreeNode<T>* GetRightChild()
 		{
 			return right_node;
 		}
-		virtual TreeNode* GetParentNode()
+		virtual TreeNode<T>* GetParentNode()
 		{
 			return parent_node;
 		}
-		virtual void SetLeftChild(TreeNode* left_child)
+		virtual void SetLeftChild(TreeNode<T>* left_child)
 		{
 			left_node = left_child;
 		}
-		virtual void SetRightChild(TreeNode* right_child)
+		virtual void SetRightChild(TreeNode<T>* right_child)
 		{
 			right_node = right_child;
 		}
-		virtual void SetParentNode(TreeNode* parent)
+		virtual void SetParentNode(TreeNode<T>* parent)
 		{
 			parent_node = parent;
 		}
-		virtual T GetData()
+		virtual void SetData(T data)
+		{
+			Data = data;
+		}
+		virtual T& GetData()
 		{
 			return Data;
 		}
 	protected:
-		TreeNode* left_node = nullptr, * right_node = nullptr, * parent_node = this;
+		TreeNode<T>* left_node = nullptr, * right_node = nullptr, * parent_node = nullptr;
 		T Data;
 	private:
 	};
 
+	/// <summary>
+	/// 为了管理树形结构的指针链接，保证能够有效地删除，树的所有指针都应该被保存，不使用该结构体后，应该统一删除
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
 	template<typename T>
 	class BinaryTree
 	{
 	public:
+		BinaryTree() {}
+		~BinaryTree()
+		{
+			for (auto& node : m_tree)
+			{
+				delete node;
+			}
+		}
 		/// <summary>
 		/// 插入设置好子关系以及父关系的节点来建立
 		/// </summary>
 		/// <param name="input_node"></param>
-		virtual void BuildByAdding(TreeNode<T>& input_node)
+		virtual void BuildByAdding(TreeNode<T>* input_node)
 		{
-			tree.emplace_back(input_node);
+			m_tree.push_back(input_node);
 		}
-		virtual void BuildTree() {};
 		/// <summary>
 		/// 设置根节点
 		/// </summary>
 		/// <param name="node">T类型表示的根节点</param>
-		virtual void SetRootNode(T node)
+		virtual void SetRootNode(TreeNode<T>* node)
 		{
-			root = &node;
+			m_root = node;
 		}
 		/// <summary>
 		/// 非递归版本的前序遍历
 		/// </summary>
 		/// <returns>前序遍历序列</returns>
-		virtual std::vector<T> preorderTraversal(void(*)(T) interfere_action)
+		virtual std::vector<T> preorderTraversal()
 		{
 			std::stack<TreeNode<T>*> sk;
 			std::vector<T> v;
-			if (root == nullptr)return v;//如果连根都没有，就直接返回空序列
-			sk.push(root);//栈的初始化
-			TreeNode<T>* next;
+			sk.push(m_root);//栈的初始化
 			while (!sk.empty())//否则必定起码有一个根节点作为初始节点
 			{
-				next = sk.top();//用临时变量保存弹出的元素
+				auto next = sk.top();//用临时变量保存弹出的元素
 				v.push_back(next->GetData());//把弹出元素的内容保存到vector上
-
-				interfere_action(next->GetData()); // 入栈时干预
-
 				sk.pop();//真正弹出该元素
-				action_first(next);
-				if (next->right != nullptr)//先压右子树
-					sk.push(next->right);
-				if (next->left != nullptr)//再压左子树
-					sk.push(next->left);
+				if (next->GetRightChild() != nullptr)//先压右子树
+					sk.push(next->GetRightChild());
+				if (next->GetLeftChild() != nullptr)//再压左子树
+					sk.push(next->GetLeftChild());
 			}
 			return v;
 		}
-
 		virtual void IndorderTraversal(TreeNode<T>& start_node)
 		{
 
@@ -109,9 +121,34 @@ namespace shabby
 		{
 
 		}
+
+		TreeNode<T>* m_root;
+		std::vector<TreeNode<T>*> m_tree;
 	private:
-		TreeNode<T>* root;
-		std::vector<TreeNode> tree;
+	};
+
+	/// <summary>
+	/// 最大堆
+	/// </summary>
+	template<typename T>
+	class GreaterHeap
+	{
+	public:
+
+	private:
+
+	};
+
+	/// <summary>
+	/// 最小堆
+	/// </summary>
+	template<typename T>
+	class LessHeap
+	{
+	public:
+
+	private:
+
 	};
 }
 #endif // !Binary_Tree_H
