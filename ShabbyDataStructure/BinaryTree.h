42a01a58f347503f1c3de6fcd186c7f5d5301437
Nomel-Cool <619471786@qq.com>
2024-03-06 14:33:31 +0800

    重构二叉树类：构造函数初始化根节点，增加静态和动态构建接口

--- ShabbyDataStructure/BinaryTree.h
+++ ShabbyDataStructure/BinaryTree.h
@@ -8,104 +8,49 @@
 #include "../ShabbyUniversalType/QuestType.h"
 #include "TopoNode.h"
 using namespace shabby;
+
 namespace shabby
 {
 	/// <summary>
 	/// 为了管理树形结构的指针链接，保证能够有效地删除，树的所有指针都应该被保存，不使用该结构体后，应该统一删除
 	/// </summary>
-	/// <typeparam name="T"></typeparam>
+	/// <typeparam name="T">节点的数据类型</typeparam>
 	template<typename T>
 	class BinaryTree
 	{
 	public:
-		BinaryTree() {}
-		~BinaryTree() {}
-		/// <summary>
-		/// 插入设置好子关系以及父关系的节点来建立
-		/// </summary>
-		/// <param name="input_node"></param>
-		virtual void BuildByAdding(TopoNode<T>* input_node)
+		BinaryTree(T data)
 		{
-			m_tree.push_back(input_node);
+			root = std::make_unique<TopoNode<T, 1, 2> >(false);
+			root->get()->SetData(data);
+			whole_tree.emplace_back(root);
 		}
+		virtual ~BinaryTree() {}
+
+		/*
+		 * 有两个方向建树
+		 * 1. 给定固定节点后，对固定节点的关系进行静态构建
+		 * 2. 在增添节点的过程中，对已有的结构进行动态构建
+		 */
+		
 		/// <summary>
-		/// 释放哈夫曼树的所有动态指针内存
+		/// 静态构建
 		/// </summary>
-		virtual void FreeTree()
-		{
-			for (TopoNode<T>* node : m_tree)
-			{
-				delete node->GetLeftChild();
-				delete node->GetRightChild();
-				delete node->GetParentNode();
-				delete node;
-			}
-		}
-		/// <summary>
-		/// 设置根节点
-		/// </summary>
-		/// <param name="node">T类型表示的根节点</param>
-		virtual void SetRootNode(TopoNode<T>* node)
-		{
-			m_root = node;
-		}
-		/// <summary>
-		/// 非递归版本的前序遍历
-		/// </summary>
-		/// <returns>前序遍历序列</returns>
-		virtual std::vector<T> preorderTraversal()
-		{
-			std::stack<TopoNode<T>*> sk;
-			std::vector<T> v;
-			sk.push(m_root);//栈的初始化
-			while (!sk.empty())//否则必定起码有一个根节点作为初始节点
-			{
-				auto next = sk.top();//用临时变量保存弹出的元素
-				v.push_back(next->GetData());//把弹出元素的内容保存到vector上
-				sk.pop();//真正弹出该元素
-				if (next->GetRightChild() != nullptr)//先压右子树
-					sk.push(next->GetRightChild());
-				if (next->GetLeftChild() != nullptr)//再压左子树
-					sk.push(next->GetLeftChild());
-			}
-			return v;
-		}
-		virtual void IndorderTraversal(TopoNode<T>& start_node)
+		virtual void build()
 		{
 
 		}
-		virtual void PostorderTraversal(TopoNode<T>& start_node)
+		/// <summary>
+		/// 动态构建
+		/// </summary>
+		/// <param name="node">加入的新节点</param>
+		virtual void build(std::unique_ptr<TopoNode<T, 1, 2> > node)
 		{
 
 		}
-
-		TopoNode<T>* m_root = nullptr;
-		std::vector<TopoNode<T, 1, 2> > m_tree;
 	private:
-	};
-
-	/// <summary>
-	/// 最大堆
-	/// </summary>
-	template<typename T>
-	class GreaterHeap
-	{
-	public:
-
-	private:
-
-	};
-
-	/// <summary>
-	/// 最小堆
-	/// </summary>
-	template<typename T>
-	class LessHeap
-	{
-	public:
-
-	private:
-
+		std::unique_ptr<TopoNode<T, 1, 2> > root;
+		std::vector<TopoNode<T, 1, 2> > whole_tree; // 支持树的所有节点可以被线性数据结构存储
 	};
 }
 #endif // !Binary_Tree_H
