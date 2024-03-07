f9d28e2068ff8739dc46ad345da647d5a7707a8f
Nomel-Cool <619471786@qq.com>
2024-03-07 15:53:06 +0800

    添加二叉树的前中后序遍历的非递归版本

--- ShabbyDataStructure/BinaryTree.h
+++ ShabbyDataStructure/BinaryTree.h
@@ -21,15 +21,15 @@ namespace shabby
 		BinaryNode() : TopoNode<T, 1, 2>() {}
 
 		/* 啰嗦一嘴：由于RVO机制，GetTheParent并不会使得返回的指针发生拷贝从而违反Uniqueptr唯一性或者引起Sharedptr引用计数增加 */
-		virtual std::shared_ptr<TopoNode<T, 1, 2> > GetLeftChildShared()
+		virtual std::shared_ptr<TopoNode<T, 1, 2> > GetLeftChild()
 		{
 			return GetTheIthChildNodeShared(0);
 		}
-		virtual std::shared_ptr<TopoNode<T, 1, 2> > GetRightChildShared()
+		virtual std::shared_ptr<TopoNode<T, 1, 2> > GetRightChild()
 		{
 			return GetTheIthChildNodeShared(1);
 		}
-		virtual std::shared_ptr<TopoNode<T, 1, 2> > GetTheParentShared()
+		virtual std::shared_ptr<TopoNode<T, 1, 2> > GetTheParent()
 		{
 			return GetTheIthParentNodeShared(0);
 		}
@@ -141,51 +141,51 @@ namespace shabby
 		/// <returns></returns>
 		virtual std::vector<BinaryNode> PreorderTraverse()
 		{
-			std::stack<std::unique_ptr<BinaryNode> > sk;
+			std::stack<std::shared_ptr<BinaryNode> > sk;
 			std::vector<BinaryNode> v;
 			if (uroot == nullptr)return v;//如果连根都没有，就直接返回空序列
 			sk.push(uroot);//栈的初始化
-			std::unique_ptr<BinaryNode> next;
+			std::shared_ptr<BinaryNode> next;
 			while (!sk.empty())//否则必定起码有一个根节点作为初始节点
 			{
 				next = sk.top();//用临时变量保存弹出的元素
-				v.push_back(next->val);//把弹出元素的内容保存到vector上
+				v.push_back(next->GetData());//把弹出元素的内容保存到vector上
 				sk.pop();//真正弹出该元素
-				if (next->right != nullptr)//先压右子树
-					sk.push(next->right);
-				if (next->left != nullptr)//再压左子树
-					sk.push(next->left);
+				if (next->GetRightChild() != nullptr)//先压右子树
+					sk.push(next->GetRightChild());
+				if (next->GetLeftChild() != nullptr)//再压左子树
+					sk.push(next->GetLeftChild());
 			}
 			return v;
 		}
 
 		virtual std::vector<BinaryNode> InorderTraverse()
 		{
-			std::stack<TreeNode*> sk;
+			std::stack<std::shared_ptr<BinaryNode>> sk;
 			std::vector<BinaryNode> v;
-			if (root == nullptr)return v;
-			sk.push(root);
-			TreeNode* temp = sk.top(), * record = nullptr;
+			if (sroot == nullptr)return v;
+			sk.push(sroot);
+			std::shared_ptr<BinaryNode> temp = sk.top(), record = nullptr;
 			while (!sk.empty())//压栈模式
 			{
-				if (temp->left != nullptr)
+				if (temp->GetLeftChild() != nullptr)
 				{
-					sk.push(temp->left);
-					temp = temp->left;
+					sk.push(temp->GetLeftChild());
+					temp = temp->GetLeftChild();
 					continue;
 				}
 				while (!sk.empty())//弹栈模式
 				{
 					temp = sk.top();//这句话放在这里，可以免去判断弹栈模式时，弹完了会越界的问题
-					if (temp->right != nullptr)//如果发现回溯过程，回溯到的节点具有右子树，则用break返回到压栈模式。
+					if (temp->GetRightChild() != nullptr)//如果发现回溯过程，回溯到的节点具有右子树，则用break返回到压栈模式。
 					{
-						v.push_back(temp->val);
+						v.push_back(temp->GetData());
 						sk.pop();
-						temp = temp->right;
+						temp = temp->GetRightChild();
 						sk.push(temp);
 						break;
 					}
-					v.push_back(temp->val);//否则该回溯的节点应该直接输出，再等待下一次的回溯。
+					v.push_back(temp->GetData());//否则该回溯的节点应该直接输出，再等待下一次的回溯。
 					sk.pop();//pop完之后，下一次的回溯就让temp=新的栈顶了，从而避免了再次往左走。
 				}
 			}
@@ -194,18 +194,18 @@ namespace shabby
 
 		virtual std::vector<BinaryNode> PostorderTraverse()
 		{
-			std::stack<TreeNode*> sk, last;
+			std::stack<std::shared_ptr<BinaryNode> > sk, last;
 			std::vector<BinaryNode> v;
-			if (root == nullptr)return v;
-			sk.push(root);
+			if (sroot == nullptr)return v;
+			sk.push(sroot);
 			last.push(nullptr);//设置一个保存转折点的栈，用于回溯时提供证据，因为如果不提供则会在转折点上产生死循环，且初始化一定是空指针
-			TreeNode* temp = sk.top();
+			std::shared_ptr<BinaryNode> temp = sk.top();
 			while (!sk.empty())
 			{
-				if (temp->left != nullptr)
+				if (temp->GetLeftChild() != nullptr)
 				{
-					sk.push(temp->left);
-					temp = temp->left;
+					sk.push(temp->GetLeftChild());
+					temp = temp->GetLeftChild();
 					continue;
 				}
 				while (!sk.empty())
@@ -213,21 +213,21 @@ namespace shabby
 					temp = sk.top();//第一个要弹出的肯定是已经搜索过的
 					if (last.top() == temp)//如果会回溯到转折点，直接弹出，返回到转折点上的单偏树继续进行回溯
 					{
-						v.push_back(temp->val);
+						v.push_back(temp->GetData());
 						sk.pop();
 						last.pop();
 						continue;
 					}
-					if (temp->right != nullptr)
+					if (temp->GetRightChild() != nullptr)
 					{
 						/*v.push_back(temp->val);*/
 						/*sk.pop();///这句说明了后序遍历和中序遍历的差别就在转折元素上*/
 						last.push(temp);//保存转折点
-						temp = temp->right;
+						temp = temp->GetRightChild();
 						sk.push(temp);
 						break;
 					}
-					v.push_back(temp->val);
+					v.push_back(temp->GetData());
 					sk.pop();
 				}
 			}
