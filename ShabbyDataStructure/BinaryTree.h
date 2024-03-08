710d44461fc35054a442aa48d6c0375efa9fc646
Nomel-Cool <619471786@qq.com>
2024-03-08 10:28:20 +0800

    修改二叉树的增，删接口。增加树结构的获取接口

--- ShabbyDataStructure/BinaryTree.h
+++ ShabbyDataStructure/BinaryTree.h
@@ -54,10 +54,6 @@ namespace shabby
 			SetTheIthParentNode(node, 0);
 		}
 
-		bool GetEdgeType()
-		{
-			return edge_type;
-		}
 	private:
 	};
 
@@ -81,40 +77,49 @@ namespace shabby
 		virtual ~BinaryTree() {}
 		
 		/// <summary>
-		/// 多边动态构建
-		/// 由于初始化时用户总是持有一个根节点，所以用户执行建树后始终可以持有一个有效的树容器，用于持续获取节点
+		/// 使用拷贝而非引用，因为p_root的生命周期可能会很短暂，但树结构可能会长一点
 		/// </summary>
-		/// <param name="current_mod_node">正在修改的节点</param>
-		/// <param name="node">加入的新节点</param>
-		/// <returns>返回当前建好的树，如果加入的节点的边属性不匹配则返回空容器</returns>
-		virtual std::vector<std::shared_ptr<BinaryNode> >& insert(std::shared_ptr<BinaryNode<T> >& current_mod_node, const BinaryNode<T>& node)
+		/// <param name="p_root">根节点智能指针</param>
+		virtual void SetRootNode(std::shared_ptr<BinaryNode<T> > p_root)
 		{
-			if (!node.GetEdgeType())
-				return std::vector<std::shared_ptr<BinaryNode<T> > >();
-			auto find_result = std::find_if(swhole_tree.begin(), swhole_tree.end(), &current_mod_node{ // 由于使用了find_if，所以复杂度为1+2+...+N = O(N²)
-				return std::vector<std::shared_ptr<BinaryNode<T> > >();
-				});
-			if (find_result == swhole_tree.end())
-				return std::vector<std::shared_ptr<BinaryNode<T> > >();
-			current_mod_node = std::make_shared<BinaryNode<T> >(node);
+			if(p_root != nullptr)
+				sroot = p_root;
+		}
+
+		virtual std::vector<std::shared_ptr<BinaryNode<T> > > GetTreeStruct()
+		{
+			return swhole_tree;
+		}
+
+		/// <summary>
+		/// 多边动态构建
+		/// 要求你传入一个确定好父，子关系的节点。具体由调用者决定
+		/// </summary>
+		/// <param name="node">加入的节点</param>
+		/// <returns>返回当前建好的树，如果加入的节点的边属性不匹配则返回空容器</returns>
+		virtual std::vector<std::shared_ptr<BinaryNode> >& insert(const BinaryNode<T>& node)
+		{
+			auto current_mod_node = std::make_shared<BinaryNode<T> >(node);
+			swhole_tree.emplace_back(current_mod_node);
 			return swhole_tree;
 		}
 
 		/// <summary>
 		/// 删除树中指定节点
+		/// 我觉得还可以再优化一下，二叉树节点的关系的释放可以放到BinaryNode的析构函数中处理，而这里只需要使用shared_ptr的reset就可以了
 		/// </summary>
 		/// <param name="current_mod_node">正在修改的节点</param>
 		/// <returns>返回当前建好的树</returns>
-		virtual std::vector<std::shared_ptr<BinaryNode<T> > >& del(std::shared_ptr<BinaryNode<T> >& current_mod_node)
+		virtual std::vector<std::shared_ptr<BinaryNode<T> > >& del(std::shared_ptr<BinaryNode<T> >& p_node)
 		{
 			// 操作其父节点删除它自己
 			auto parent_node = current_mod_node->GetTheParentUnique();
-			if (parent_node->GetLeftChild() == current_mod_node)
+			if (parent_node->GetLeftChild() == p_node)
 			{
 				auto nulltmp = std::make_shared<BinaryNode<T> >(nullptr);
 				parent_node->SetLeftChild(nulltmp);
 			}
-			else if (parent_node->GetRightChild() == current_mod_node)
+			else if (parent_node->GetRightChild() == p_node)
 			{
 				auto nulltmp = std::make_shared<BinaryNode<T> >(nullptr);
 				parent_node->SetRightChild(nulltmp);
@@ -125,11 +130,20 @@ namespace shabby
 
 			// 删除左子树
 			auto nulltmpl = std::make_shared<BinaryNode<T> >(nullptr);
-			current_mod_node->SetLeftChild(nulltmpl);
+			p_node->SetLeftChild(nulltmpl);
 
 			// 删除右子树
 			auto nulltmpr = std::make_shared<BinaryNode<T> >(nullptr);
-			current_mod_node->SetRightChild(nulltmpr);
+			p_node->SetRightChild(nulltmpr);
+
+			// 在树中删除它
+			auto find_result = std::find_if(swhole_tree.begin(), swhole_tree.end(), p_node);
+			if (find_result == swhole_tree.end())
+				return std::vector<std::shared_ptr<BinaryNode<T> > >();
+			else
+				swhole_tree.erase(find_result);
+
+			return swhole_tree;
 		}
 
 		/// <summary>
