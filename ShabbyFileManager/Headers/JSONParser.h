b51a9e4f49af58fc028784a48b19644bc2c5a1c4
Nomel-Cool <619471786@qq.com>
2024-11-28 14:53:59 +0800

    添加JSON和XML解析器

---
+++ ShabbyFileManager/Headers/JSONParser.h
@@ -0,0 +1,18 @@
+#ifndef JSON_PARSER_H
+#define JSON_PARSER_H
+
+#include <iostream>
+#include <string>
+
+template<typename StructuringClass>
+class JsonParser
+{
+public:
+
+protected:
+
+private:
+
+};
+
+#endif // !JSON_PARSER_H
---
+++ ShabbyFileManager/Headers/XMLParser.h
@@ -0,0 +1,19 @@
+#ifndef XML_PARSER_H
+#define XML_PARSER_H
+
+#include <iostream>
+#include <string>
+#include "tinyxml2.h"
+
+template<typename StructuringClass>
+class XmlParser
+{
+public:
+
+protected:
+
+private:
+
+};
+
+#endif // !XML_PARSER_H
