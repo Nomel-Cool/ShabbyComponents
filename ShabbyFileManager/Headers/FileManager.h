a88fe217d5df7b4a6908b7221dafd8232e606aa9
Nomel-Cool <619471786@qq.com>
2024-11-28 19:07:12 +0800

    初步完成XML反序列化的功能编写

--- ShabbyFileManager/Headers/FileManager.h
+++ ShabbyFileManager/Headers/FileManager.h
@@ -2,6 +2,9 @@
 #ifndef FILE_MANAGER_H
 #define FILE_MANAGER_H
 #include <filesystem>
+#include <string>
+#include "XMLParser.h"
+
 /// <summary>
 /// 封装filesystem库，用于
 /// 1. 查询某目录下的指定文件数量，种类，以及访问权限
@@ -12,8 +15,43 @@ class FileManager
 {
 public:
 	FileManager() {}
-private:
 
+	/// <summary>
+	/// XML反序列化为类始终是会需要一些代价的，DeSerialize函数通过map存储关注的string字段（详见DeSerialize说明）
+	/// 从而使得XML可以利用处理string字段来与其它类沟通
+	/// 但是TransXml2Class并不知道转换的协议，也不应该知道，这部分的代价由可由XML反序列化的类实现来承担，即FillUp函数以及其它类内可能需要的信号量
+	/// 也许有更优雅的处理方式，但这样实现在模块分离时至少不会影响FileManage类以及可反序列化类（因为FillUp函数和对应的信号量可以被弃用）
+	/// </summary>
+	/// <typeparam name="StructuringClass">定义了公开的FillUp的可序列化类</typeparam>
+	/// <param name="xml_path">xml文件的路径</param>
+	/// <param name="target_obj">可序列化类的实例</param>
+	/// <returns>反序列化成功target_obj被填充为true，否则false</returns>
+	template<typename StructuringClass>
+	bool TransXml2Class(std::string xml_path, StructuringClass& target_obj)
+	{
+		auto deserialized_data = DeSerialize(xml_path, target_obj);
+		if (!deserialized_data.empty())
+			try
+			{
+				if constexpr (std::is_invocable_v<decltype(&StructuringClass::FillUp), StructuringClass, std::string>)
+					for (const auto& [element, attributes] : deserialized_data)
+					{
+						target_obj.FillUp(element);
+						for (const auto& [attr, value] : attributes)
+						{
+							target_obj.FillUp(attr);
+							target_obj.FillUp(value);
+						}
+					}
+				else
+					throw std::logic_error("The class does not implement the required FillUp(std::string) function.");
+				return true;
+			}
+		std::cerr << "DeSerialization failed or returned empty data." << std::endl;
+		return false;
+	}
+private:
+	XmlParser xml_parser;
 };
 
 #endif
