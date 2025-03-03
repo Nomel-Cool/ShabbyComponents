aa15ef6f07e61f811ddcc823278efbe18911039d
Nomel-Cool <619471786@qq.com>
2025-03-03 19:21:49 +0800

    修改Redis 客户端类为dll导出，而不是lib导出

--- ShabbyRedis/Headers/RedisClient.h
+++ ShabbyRedis/Headers/RedisClient.h
@@ -1,4 +1,13 @@
 #pragma once
+
+#ifndef SHABBY_REDIS_CLIENT_EXPORTS
+#define SHABBY_CLASS __declspec(dllimport)
+#define SHABBY_TEMPLATE
+#else //EXPORT
+#define SHABBY_CLASS __declspec(dllexport)
+#define SHABBY_TEMPLATE __declspec(dllexport)
+#endif
+
 #ifndef REDIS_CLIENT
 #define REDIS_CLIENT
 
@@ -7,7 +16,7 @@
 #include <functional>
 #include <hiredis.h>
 
-class RedisClient {
+class SHABBY_CLASS  RedisClient {
 public:
 
     RedisClient();
